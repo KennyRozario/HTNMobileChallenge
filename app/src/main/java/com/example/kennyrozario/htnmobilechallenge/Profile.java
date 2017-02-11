4cb5937386d9b52aa395f8aa344a82af26b07e68
Kenny Rozario <kmrozario98@gmail.com>
2017-02-10 19:24:50 -0500

    Added Profile Object and OkHttp to gradle

---
+++ app/src/main/java/com/example/kennyrozario/htnmobilechallenge/Profile.java
@@ -0,0 +1,82 @@
+package com.example.kennyrozario.htnmobilechallenge;
+
+import java.util.HashMap;
+
+/**
+ * Created by Kenny on 2017-02-10.
+ */
+
+public class Profile {
+    private String mCompany;
+    private String mEmail;
+    private double mLat;
+    private double mLong;
+    private String mName;
+    private String mPhone;
+    private String mPicture;
+    private HashMap <String, Integer> mSkills;
+
+    public String getCompany() {
+        return mCompany;
+    }
+
+    public void setCompany(String company) {
+        mCompany = company;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String email) {
+        mEmail = email;
+    }
+
+    public double getLat() {
+        return mLat;
+    }
+
+    public void setLat(double lat) {
+        mLat = lat;
+    }
+
+    public double getLong() {
+        return mLong;
+    }
+
+    public void setLong(double aLong) {
+        mLong = aLong;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public void setPhone(String phone) {
+        mPhone = phone;
+    }
+
+    public String getPicture() {
+        return mPicture;
+    }
+
+    public void setPicture(String picture) {
+        mPicture = picture;
+    }
+
+    public HashMap<String, Integer> getSkills() {
+        return mSkills;
+    }
+
+    public void setSkills(HashMap<String, Integer> skills) {
+        mSkills = skills;
+    }
+}
